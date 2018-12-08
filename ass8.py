6f346990cdb9e3dbacf9ad5a447ad46ba33cfab3
MUHAMMED NABEEL M <nabeelin1994@gmail.com>
2018-12-09 01:48:47 +0530

    Add files via upload
    Given the area of the circle, write a program to print the radius

---
+++ ass8.py
@@ -0,0 +1,18 @@
+
+# import the math module  
+import math
+
+
+# Function to print radi
+
+def update(a):
+    b=a/3.14
+    c=math.sqrt(b)
+
+    print("Radius of your circle  :", c)
+
+
+
+a=float(input("Enter area of Circle  : "))
+
+update(a)
